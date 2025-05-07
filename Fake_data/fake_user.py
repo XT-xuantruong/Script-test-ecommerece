7d6cefd579c7c4d67b08fdaa2817dc037df5e626
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-05-07 07:28:10 +0700

    feat: add scripts for generating fake user data and keywords

---
+++ Fake_data/fake_user.py
@@ -0,0 +1,25 @@
+import bcrypt
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="truong12345",
+    password="truong12345",
+    port=3306,
+    database="ecommerce_api"
+)
+cursor = conn.cursor()
+
+password = bcrypt.hashpw("password123".encode(), bcrypt.gensalt()).decode()
+for i in range(10000):
+    print(i)
+    username = "User" + str(i + 1)
+    email = "user" + str(i + 1) + "@example.com"
+    cursor.execute(
+        "INSERT INTO users (email, full_name, password, role_id, avatar, phone, address, created_at) "
+        "VALUES (%s, %s, %s, %s, %s, %s, %s, NOW())",
+        (email, username, password, 3, "", "", "")
+    )
+conn.commit()
+cursor.close()
+conn.close()
